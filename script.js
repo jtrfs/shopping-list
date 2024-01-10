c68dada13232fdbc2d6ac19d289c804f8eb6d602
Jan Varmuza <jan.varmuza@gmail.com>
2024-01-10 19:12:24 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e) {
+  e.preventDefault();
+  const newItem = itemInput.value;
+  if (newItem === '') {
+    alert('Enter an item!');
+    return;
+  }
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+  itemList.appendChild(li);
+  itemInput.value = '';
+}
+
+function createButton(classes) {
+  const btn = document.createElement('button');
+  btn.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  btn.appendChild(icon);
+  return btn;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+// event listeners
+itemForm.addEventListener('submit', addItem);
